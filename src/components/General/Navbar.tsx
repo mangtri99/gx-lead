facb099b389d4bf48ffd627f88b36d6581071e64
Mang Tri <mangtri93@gmail.com>
2024-02-20 17:14:10 +0800

    feat: fixed navbar

--- src/components/General/Navbar.tsx
+++ src/components/General/Navbar.tsx
@@ -7,10 +7,12 @@ import { useLocation } from "react-router-dom";
 import { useAuth } from "../../composables/useAuth";
 import ButtonActionIcon from "../Button/ButtonActionIcon";
 import { LayoutContext } from "../../layouts/context/LayoutContext";
-
+import { useMedia } from "react-use";
+import { SIDEBAR_WIDTH } from "../../config/general";
 
 export default function Navbar() {
   const { isShowSidebar, setIsShowSidebar } = useContext(LayoutContext);
+  const isMobile = useMedia('(max-width: 992px)');
   const [title, setTitle] = useState("Dashboard");
   const location = useLocation();
   const { logout } = useAuth();
@@ -30,59 +32,71 @@ export default function Navbar() {
   }, [location.pathname]);
 
   return (
-    <div className="bg-white shadow-sm p-3">
-      <div className="d-flex align-items-center justify-content-between">
-        <div className="d-flex align-items-center">
-          {/* Desktop */}
-          <ButtonActionIcon className="d-none d-lg-flex" onClick={() => setIsShowSidebar(!isShowSidebar)}>
-            <IoMenu size={24} />
-          </ButtonActionIcon>
-
-          {/* Mobile */}
-          <ButtonActionIcon
-            className="d-lg-none"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasExample"
-            aria-controls="offcanvasExample"
-          >
-            <IoMenu size={24} />
-          </ButtonActionIcon>
-
-          <p className="nav-title ms-3">
-            {title}
-          </p>
-        </div>
-        <div className="d-flex align-items-center">
-          <ButtonActionIcon className="p-2 me-2">
-            <GoBell size={24} />
-          </ButtonActionIcon>
-          <div className="dropdown">
+    <div
+      className="fixed-top main-content"
+      style={{
+        marginLeft: isShowSidebar && !isMobile ? `${SIDEBAR_WIDTH}px` : '0px',
+      }}
+    >
+      <div className="w-100 bg-white shadow-sm p-3">
+        <div className="d-flex align-items-center justify-content-between">
+          <div className="d-flex align-items-center">
+            {/* Desktop */}
             <ButtonActionIcon
-              className="p-2"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+              className="d-none d-lg-flex"
+              onClick={() => setIsShowSidebar(!isShowSidebar)}
             >
-              <div className="d-flex align-items-center">
-                <img
-                  src={Profile}
-                  alt="Profile"
-                  className="rounded-circle"
-                  style={{
-                    width: "30px",
-                    height: "30px",
-                  }}
-                />
-                <RiArrowDropDownFill size={24} />
-              </div>
+              <IoMenu size={24} />
             </ButtonActionIcon>
-            <ul className="dropdown-menu">
-              <li>
-                <a role="button" className="dropdown-item" onClick={()=> logout()}>
-                  Logout
-                </a>
-              </li>
-            </ul>
+
+            {/* Mobile */}
+            <ButtonActionIcon
+              className="d-lg-none"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasExample"
+              aria-controls="offcanvasExample"
+            >
+              <IoMenu size={24} />
+            </ButtonActionIcon>
+
+            <p className="nav-title ms-3">{title}</p>
+          </div>
+          <div className="d-flex align-items-center">
+            <ButtonActionIcon className="p-2 me-2">
+              <GoBell size={24} />
+            </ButtonActionIcon>
+            <div className="dropdown">
+              <ButtonActionIcon
+                className="p-2"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <div className="d-flex align-items-center">
+                  <img
+                    src={Profile}
+                    alt="Profile"
+                    className="rounded-circle"
+                    style={{
+                      width: "30px",
+                      height: "30px",
+                    }}
+                  />
+                  <RiArrowDropDownFill size={24} />
+                </div>
+              </ButtonActionIcon>
+              <ul className="dropdown-menu">
+                <li>
+                  <a
+                    role="button"
+                    className="dropdown-item"
+                    onClick={() => logout()}
+                  >
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
